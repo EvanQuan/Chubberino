b4521f5bcea97c12f22e8da1df0f656d7da36a08
Evan Quan <evanlquan@gmail.com>
2020-07-12 23:14:48 -0600

    Add interval to Repeat

--- Chubberino/Client/Commands/Settings/Repeat.cs
+++ Chubberino/Client/Commands/Settings/Repeat.cs
@@ -1,6 +1,8 @@
 ﻿using Chubberino.Client.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms.Automation;
 using TwitchLib.Client.Interfaces;
 
 namespace Chubberino.Client.Commands.Settings
@@ -12,20 +14,43 @@ namespace Chubberino.Client.Commands.Settings
     /// </summary>
     internal sealed class Repeat : Setting
     {
+        public TimeSpan RepeatInterval { get; set; }
         private String RepeatMessage { get; set; }
         public Repeat(ITwitchClient client, IMessageSpooler spooler)
             : base(client, spooler)
         {
         }
 
-        public override String Status => String.IsNullOrWhiteSpace(RepeatMessage)
+        public override String Status => (String.IsNullOrWhiteSpace(RepeatMessage)
             ? "disabled"
-            : RepeatMessage;
+            : RepeatMessage)
+            + $" Interval: {RepeatInterval.TotalSeconds} seconds";
 
         public override void Execute(IEnumerable<String> arguments)
         {
             RepeatMessage = String.Join(" ", arguments);
             Spooler.RepeatMessage = RepeatMessage;
+        }
+
+        public override Boolean Set(String value, IEnumerable<String> arguments)
+        {
+            switch (value.ToLower())
+            {
+                case "i":
+                case "interval":
+                {
+                    if (Double.TryParse(arguments.FirstOrDefault(), out Double result))
+                    {
+                        RepeatInterval = result >= 0
+                            ? TimeSpan.FromSeconds(result)
+                            : TimeSpan.Zero;
+
+                        return true;
+                    }
+                }
+                break;
+            }
+            return false;
         }
     }
 }
