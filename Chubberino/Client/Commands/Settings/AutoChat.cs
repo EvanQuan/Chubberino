561085af7ba1e5f70b953efbe5b3e2c3f52db3c7
Evan Quan <evanlquan@gmail.com>
2020-08-02 23:39:06 -0600

    Add Spam increaser

--- Chubberino/Client/Commands/Settings/AutoChat.cs
+++ Chubberino/Client/Commands/Settings/AutoChat.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using TwitchLib.Client.Events;
 using TwitchLib.Client.Interfaces;
 using TwitchLib.Client.Models;
 
@@ -12,19 +13,22 @@ namespace Chubberino.Client.Commands.Settings
     {
         private ConcurrentQueue<String> PreviousMessages { get; }
 
-        private Int32 MessageCount { get; set; } = 25;
+        private Int32 SpamMessageCount { get; set; } = 10;
+
+        private Int32 GeneralMessageCount { get; set; } = 25;
 
         public override String Status => base.Status
-            + $"\n\tCount: {MessageCount}";
+            + $"\n\tSpam count: {SpamMessageCount}"
+            + $"\n\tGeneral count: {GeneralMessageCount}";
 
         public AutoChat(ITwitchClient client, IMessageSpooler spooler)
             : base(client, spooler)
         {
             TwitchClient.OnMessageReceived += TwitchClient_OnMessageReceived;
-            PreviousMessages = new ConcurrentQueue<string>();
+            PreviousMessages = new ConcurrentQueue<String>();
         }
 
-        private void TwitchClient_OnMessageReceived(Object sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
+        private void TwitchClient_OnMessageReceived(Object sender, OnMessageReceivedArgs e)
         {
             if (!IsEnabled) { return; }
 
@@ -32,17 +36,12 @@ namespace Chubberino.Client.Commands.Settings
 
             PreviousMessages.Enqueue(e.ChatMessage.Message);
 
-            if (PreviousMessages.Count >= MessageCount)
+            if (PreviousMessages.Count >= GeneralMessageCount)
             {
-                // Get the most common message from the last MessageCount
+                // Get the most common message from the last GeneralMessageCount
                 // number of messages, as long as there is more than one
                 // duplicate of it.
-                String message = PreviousMessages
-                    .GroupBy(x => x)
-                    .OrderByDescending(x => x.Count()).ThenBy(x => x.Key)
-                    .Where(x => x.Count() > 1)
-                    .Select(x => x.Key)
-                    .FirstOrDefault();
+                String message = GetMessageToSend(3);
 
                 if (message != null)
                 {
@@ -53,31 +52,40 @@ namespace Chubberino.Client.Commands.Settings
                 // avoid copying old messages.
                 PreviousMessages.Clear();
             }
+            else if (PreviousMessages.Count >= SpamMessageCount)
+            {
+                // Get the most common message from the last SpamMessageCount
+                // number of messages, as long as there is more than one
+                // duplicate of it.
+                String message = GetMessageToSend(4);
+
+                if (message != null)
+                {
+                    Spooler.SpoolMessage(message);
+                    // Empty the last messages, if if one was sent.
+                    PreviousMessages.Clear();
+                }
+
+            }
         }
 
-        public override Boolean Set(String property, IEnumerable<String> arguments)
+        private String GetMessageToSend(Int32 duplicateCount)
         {
-            switch (property)
-            {
-                case "c":
-                case "count":
-                    if (Int32.TryParse(arguments.FirstOrDefault(), out Int32 result))
-                    {
-                        MessageCount = result;
-                        return true;
-                    }
-                    return false;
-            }
-
-            return false;
+            return PreviousMessages
+                .GroupBy(x => x)
+                .OrderByDescending(x => x.Count()).ThenBy(x => x.Key)
+                .Where(x => x.Count() >= duplicateCount)
+                .Select(x => x.Key)
+                .FirstOrDefault();
         }
 
         private Boolean ShouldCountUser(ChatMessage chatMessage)
         {
-            return !chatMessage.IsMe
+            return !chatMessage.Username.Equals(chatMessage.BotUsername)
                 && !chatMessage.IsModerator
                 && !chatMessage.IsVip
-                && !chatMessage.Message.StartsWith('!'); // Ignore channel bot commands
+                && !chatMessage.Message.StartsWith('!') // Ignore channel bot commands
+                && !chatMessage.Message.Contains(TwitchInfo.BotUsername, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
