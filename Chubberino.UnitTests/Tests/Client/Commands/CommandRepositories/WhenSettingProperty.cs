dca7f87abe7e45061b3e4067bdc8385c905e191e
Evan Quan <evanlquan@gmail.com>
2020-09-29 17:52:26 -0600

    Add WhenSettingProperty.ShouldOutputCommandNotFoundMessage

--- Chubberino.UnitTests/Tests/Client/Commands/CommandRepositories/WhenSettingProperty.cs
+++ Chubberino.UnitTests/Tests/Client/Commands/CommandRepositories/WhenSettingProperty.cs
@@ -12,6 +12,7 @@ namespace Chubberino.UnitTests.Tests.Client.Commands.CommandRepositories
     public sealed class WhenSettingProperty : UsingCommandRepository
     {
         /// <summary>
+        /// Should output a message indicating the property failed to be set.
         /// A property is not set if the value being set is invalid, or if the
         /// property cannot be found.
         /// </summary>
@@ -33,6 +34,26 @@ namespace Chubberino.UnitTests.Tests.Client.Commands.CommandRepositories
             Sut.Execute("set", commandWithArguments);
 
             MockedConsole.Verify(x => x.WriteLine($"Command \"{validCommandName}\" property \"{propertyName}\" not set."), Times.Once());
+        }
+
+        /// <summary>
+        /// Should output a message indicating the command was not found.
+        /// </summary>
+        /// <param name="arguments"></param>
+        [Theory]
+        [InlineData("a", "1")]
+        [InlineData("a", "b")]
+        [InlineData("s", "a")]
+        public void ShouldOutputCommandNotFoundMessage(params String[] arguments)
+        {
+            String invalidCommandName = Guid.NewGuid().ToString();
+            String propertyName = arguments[0];
+            List<String> commandWithArguments = new List<String>() { invalidCommandName };
+            commandWithArguments.AddRange(arguments);
+
+            Sut.Execute("set", commandWithArguments);
+
+            MockedConsole.Verify(x => x.WriteLine($"Command \"{invalidCommandName}\" not found to set."), Times.Once());
         }
     }
 }
