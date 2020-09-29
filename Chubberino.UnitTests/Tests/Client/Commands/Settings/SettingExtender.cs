da473e232bdb319488ce92f05ec67bbffd4d598b
Evan Quan <evanlquan@gmail.com>
2020-09-29 17:25:45 -0600

    Add SettingExtender

---
+++ Chubberino.UnitTests/Tests/Client/Commands/Settings/SettingExtender.cs
@@ -0,0 +1,17 @@
+﻿using Chubberino.Client.Abstractions;
+using Chubberino.Client.Commands.Settings;
+using System.IO;
+
+namespace Chubberino.UnitTests.Tests.Client.Commands.Settings
+{
+    /// <summary>
+    /// Extends <see cref="Setting"/> to be instantiated.
+    /// </summary>
+    public sealed class SettingExtender : Setting
+    {
+        public SettingExtender(IExtendedClient client, TextWriter console)
+            : base(client, console)
+        {
+        }
+    }
+}
