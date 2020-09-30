2bc6c96a1b462c1047d109d9de3f8bee6ec507d0
Evan Quan <evanlquan@gmail.com>
2020-09-30 03:10:46 -0600

    Remove redundant property.

--- Chubberino.UnitTests/Tests/Client/Commands/Settings/DisableAlls/UsingDisableAll.cs
+++ Chubberino.UnitTests/Tests/Client/Commands/Settings/DisableAlls/UsingDisableAll.cs
@@ -11,12 +11,8 @@ namespace Chubberino.UnitTests.Tests.Client.Commands.Settings.DisableAlls
         /// </summary>
         protected DisableAll Sut { get; }
 
-        protected Mock<ICommandRepository> MockedCommandRepository { get; }
-
         public UsingDisableAll()
         {
-            MockedCommandRepository = new Mock<ICommandRepository>();
-
             Sut = new DisableAll(MockedTwitchClient.Object, MockedCommandRepository.Object, MockedConsole.Object);
         }
     }
