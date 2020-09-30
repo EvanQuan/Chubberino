ed46e153f9b39453d7328662f4fa3172f8c7a068
Evan Quan <evanlquan@gmail.com>
2020-09-30 04:44:43 -0600

    Add WhenDisposing

---
+++ Chubberino.UnitTests/Tests/Client/Bots/WhenDisposing.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using Xunit;
+
+namespace Chubberino.UnitTests.Tests.Client.Bots
+{
+    public sealed class WhenDisposing : UsingBot
+    {
+        [Fact]
+        public void ShouldDisconnect()
+        {
+            MockedClient.Setup(x => x.IsConnected).Returns(true);
+
+            Sut.Dispose();
+
+            MockedClient.Verify(x => x.Disconnect(), Times.Once());
+        }
+
+        [Fact]
+        public void ShouldNotDisonnect()
+        {
+            MockedClient.Setup(x => x.IsConnected).Returns(false);
+
+            Sut.Dispose();
+
+            MockedClient.Verify(x => x.Disconnect(), Times.Never());
+        }
+    }
+}
