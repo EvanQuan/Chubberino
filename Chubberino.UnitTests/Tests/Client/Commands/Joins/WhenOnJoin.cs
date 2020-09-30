6f195e9d3b02382fe856316bf993e455afc9b173
Evan Quan <evanlquan@gmail.com>
2020-09-29 18:26:37 -0600

    Add ShouldOutputLeftAndJoinedChannelMessage

--- Chubberino.UnitTests/Tests/Client/Commands/Joins/WhenOnJoin.cs
+++ Chubberino.UnitTests/Tests/Client/Commands/Joins/WhenOnJoin.cs
@@ -34,5 +34,38 @@ namespace Chubberino.UnitTests.Tests.Client.Commands.Joins
             MockedConsole.Verify(x => x.WriteLine($"Joined channel {newChannel}"), Times.Once());
             MockedConsole.Verify(x => x.WriteLine(It.IsAny<String>()), Times.Once());
         }
+
+        /// <summary>
+        /// Should output a message indicating a channel has been joined.
+        /// With a prior joined channel, there should not a left channel
+        /// message with the old channel name.
+        /// </summary>
+        /// <param name="oldChannel"></param>
+        /// <param name="newChannel"></param>
+        [Theory]
+        [InlineData("b", "a")]
+        [InlineData("a", "b")]
+        public void ShouldOutputLeftChannelAndJoinedChannelMessages(String oldChannel, String newChannel)
+        {
+            var args1 = new OnJoinedChannelArgs()
+            {
+                Channel = oldChannel
+            };
+
+            Sut.TwitchClient_OnJoinedChannel(null, args1);
+
+            var args2 = new OnJoinedChannelArgs()
+            {
+                Channel = newChannel
+            };
+
+            Sut.TwitchClient_OnJoinedChannel(null, args2);
+
+            MockedConsole.Verify(x => x.WriteLine($"Joined channel {oldChannel}"), Times.Once());
+            MockedConsole.Verify(x => x.WriteLine($"Left channel {oldChannel}"), Times.Once());
+            MockedConsole.Verify(x => x.WriteLine($"Joined channel {newChannel}"), Times.Once());
+            MockedConsole.Verify(x => x.WriteLine(It.IsAny<String>()), Times.Exactly(3));
+        }
+
     }
 }
