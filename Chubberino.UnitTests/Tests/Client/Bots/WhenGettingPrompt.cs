5711621b510c67944b24df67905acb16c779271d
Evan Quan <evanlquan@gmail.com>
2020-09-30 05:18:36 -0600

    Add WhenGettingPrompt

---
+++ Chubberino.UnitTests/Tests/Client/Bots/WhenGettingPrompt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace Chubberino.UnitTests.Tests.Client.Bots
+{
+    public sealed class WhenGettingPrompt : UsingBot
+    {
+        [Theory]
+        [InlineData(true, "Mod")]
+        [InlineData(false, "Normal")]
+        public void ShouldContainRelevantInformation(Boolean isModerator, String expectedModeratorText)
+        {
+            BotInfo.IsModerator = isModerator;
+
+            var commandStatus = Guid.NewGuid().ToString();
+
+            MockedCommandRepository.Setup(x => x.GetStatus()).Returns(commandStatus);
+
+            String prompt = Sut.GetPrompt();
+
+            Assert.Contains(commandStatus, prompt);
+            Assert.Contains(expectedModeratorText, prompt);
+            Assert.Contains(BotInfo.ChannelName, prompt);
+        }
+    }
+}
