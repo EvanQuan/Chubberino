690a1b0f32fd59b42e711afcefc4397d8651eee1
Evan Quan <evanlquan@gmail.com>
2020-10-06 03:43:57 -0600

    Move properties to PyramidTracker

---
+++ Chubberino/Client/Commands/Settings/Pyramids/PyramidTracker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chubberino.Client.Commands.Settings.Pyramids
+{
+    public sealed class PyramidTracker
+    {
+        public HashSet<String> ContributorDisplayNames { get; }
+
+        public String Block { get; set; }
+
+        public Int32 CurrentHeight { get; set; }
+
+        public Int32 TallestHeight { get; set; }
+
+        public Boolean BuildingUp { get; set; }
+
+        public PyramidTracker()
+        {
+            ContributorDisplayNames = new HashSet<String>();
+            Reset();
+        }
+
+        public void Reset()
+        {
+            BuildingUp = true;
+            CurrentHeight = 0;
+            TallestHeight = 0;
+            Block = null;
+            ContributorDisplayNames.Clear();
+        }
+    }
+}
--- Chubberino/Client/Commands/Settings/TrackPyramids.cs
+++ Chubberino/Client/Commands/Settings/TrackPyramids.cs
@@ -1,4 +1,5 @@
 ﻿using Chubberino.Client.Abstractions;
+using Chubberino.Client.Commands.Settings.Pyramids;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,19 +15,12 @@ namespace Chubberino.Client.Commands.Settings
         /// </summary>
         private const Int32 MinimumRelevantPyramidHeight = 3;
 
-        private HashSet<String> PyramidContributorUsernames { get; }
-
-        public String PyramidBlock { get; set; }
-
-        public Int32 CurrentPyramidHeight { get; set; } = 0;
-        public Int32 TallestPyramidHeight { get; set; } = 0;
-
-        private Boolean BuildingUp { get; set; } = true;
+        private PyramidTracker Pyramid { get; }
 
         public TrackPyramids(IExtendedClient client, TextWriter console)
             : base(client, console)
         {
-            PyramidContributorUsernames = new HashSet<String>();
+            Pyramid = new PyramidTracker();
 
             Enable = twitchClient =>
             {
@@ -45,9 +39,9 @@ namespace Chubberino.Client.Commands.Settings
             if (TryGetFirstPyramidBlock(e.ChatMessage.Message, out String block))
             {
                 // If this is end of the current pyramid
-                if (block == PyramidBlock && TallestPyramidHeight >= MinimumRelevantPyramidHeight && CurrentPyramidHeight == 2)
+                if (block == Pyramid.Block && Pyramid.TallestHeight >= MinimumRelevantPyramidHeight && Pyramid.CurrentHeight == 2)
                 {
-                    PyramidContributorUsernames.Add(e.ChatMessage.DisplayName);
+                    Pyramid.ContributorDisplayNames.Add(e.ChatMessage.DisplayName);
                     SpoolPyramidSuccessMessage();
                 }
                 else
@@ -57,62 +51,54 @@ namespace Chubberino.Client.Commands.Settings
                 }
 
                 // Successfully started a new pyramid
-                PyramidBlock = block;
-                CurrentPyramidHeight = 1;
-                TallestPyramidHeight = 1;
-                PyramidContributorUsernames.Clear();
-                PyramidContributorUsernames.Add(e.ChatMessage.DisplayName);
+                Pyramid.Block = block;
+                Pyramid.CurrentHeight = 1;
+                Pyramid.TallestHeight = 1;
+                Pyramid.ContributorDisplayNames.Clear();
+                Pyramid.ContributorDisplayNames.Add(e.ChatMessage.DisplayName);
             }
-            else if (PyramidBlock != null)
+            else if (Pyramid.Block != null)
             {
                 // We already have a pyramid
 
                 String[] tokens = e.ChatMessage.Message.Split(" ");
 
-                IEnumerable<String> uncleanTokens = tokens.Where(element => element == PyramidBlock || String.IsNullOrWhiteSpace(element) || element[0] == Data.InvisibleCharacter);
+                IEnumerable<String> uncleanTokens = tokens.Where(element => element == Pyramid.Block || String.IsNullOrWhiteSpace(element) || element[0] == Data.InvisibleCharacter);
 
                 Int32 uncleanTokensCount = uncleanTokens.Count();
 
                 if (uncleanTokens.Count() == tokens.Length)
                 {
-                    Int32 cleanTokensCount = uncleanTokens.Where(element => element == PyramidBlock).Count();
+                    Int32 cleanTokensCount = uncleanTokens.Where(element => element == Pyramid.Block).Count();
 
-                    if (BuildingUp && cleanTokensCount == CurrentPyramidHeight + 1)
+                    if (Pyramid.BuildingUp && cleanTokensCount == Pyramid.CurrentHeight + 1)
                     {
                         // Continuing to build up
-                        CurrentPyramidHeight++;
-                        TallestPyramidHeight++;
-                        PyramidContributorUsernames.Add(e.ChatMessage.DisplayName);
+                        Pyramid.CurrentHeight++;
+                        Pyramid.TallestHeight++;
+                        Pyramid.ContributorDisplayNames.Add(e.ChatMessage.DisplayName);
                     }
-                    else if (cleanTokensCount == CurrentPyramidHeight - 1)
+                    else if (cleanTokensCount == Pyramid.CurrentHeight - 1)
                     {
                         // Switched to build down or continuing down
-                        BuildingUp = false;
-                        CurrentPyramidHeight--;
-                        PyramidContributorUsernames.Add(e.ChatMessage.DisplayName);
+                        Pyramid.BuildingUp = false;
+                        Pyramid.CurrentHeight--;
+                        Pyramid.ContributorDisplayNames.Add(e.ChatMessage.DisplayName);
                     }
                     else
                     {
                         // Broke the pyramid in a way that does not start a new pyramid.
-                        SpoolPyramidFailedMessage(e.ChatMessage.DisplayName);
-                        BuildingUp = true; // for the next pyramid
-                        CurrentPyramidHeight = 0;
-                        TallestPyramidHeight = 0;
-                        PyramidBlock = null;
-                        PyramidContributorUsernames.Clear();
+                        Pyramid.Reset();
                     }
                 }
                 else
                 {
                     // Still not a pyramid
-                    BuildingUp = true;
-                    CurrentPyramidHeight = 0;
-                    TallestPyramidHeight = 0;
-                    PyramidBlock = null;
-                    PyramidContributorUsernames.Clear();
+                    Pyramid.Reset();
                 }
             }
         }
+
 
         private Boolean TryGetFirstPyramidBlock(String message, out String block)
         {
@@ -128,13 +114,13 @@ namespace Chubberino.Client.Commands.Settings
 
         private void SpoolPyramidSuccessMessage()
         {
-            if (PyramidContributorUsernames.Count == 1)
+            if (Pyramid.ContributorDisplayNames.Count == 1)
             {
-                TwitchClient.SpoolMessage($"@{PyramidContributorUsernames.Single()} Nice {TallestPyramidHeight}-story tall {PyramidBlock} pyramid! peepoClap");
+                TwitchClient.SpoolMessage($"@{Pyramid.ContributorDisplayNames.Single()} Nice {Pyramid.TallestHeight}-story tall {Pyramid.Block} pyramid! peepoClap");
             }
             else
             {
-                TwitchClient.SpoolMessage($"@{String.Join(", @", PyramidContributorUsernames)} Nice {TallestPyramidHeight}-story tall {PyramidBlock} pyramid! Hooray teamwork! peepoClap");
+                TwitchClient.SpoolMessage($"@{String.Join(", @", Pyramid.ContributorDisplayNames)} Nice {Pyramid.TallestHeight}-story tall {Pyramid.Block} pyramid! Hooray teamwork! peepoClap");
             }
         }
 
@@ -144,25 +130,25 @@ namespace Chubberino.Client.Commands.Settings
         private void SpoolPyramidFailedMessage(String userThatBrokePyramid)
         {
             // Only send message on relavant pyramids to avoid false positives.
-            if (TallestPyramidHeight < MinimumRelevantPyramidHeight) { return; }
+            if (Pyramid.TallestHeight < MinimumRelevantPyramidHeight) { return; }
 
-            if (PyramidContributorUsernames.Contains(userThatBrokePyramid))
+            if (Pyramid.ContributorDisplayNames.Contains(userThatBrokePyramid))
             {
-                if (PyramidContributorUsernames.Count == 1)
+                if (Pyramid.ContributorDisplayNames.Count == 1)
                 {
                     // User broke their own pyramid.
-                    TwitchClient.SpoolMessage($"@{userThatBrokePyramid} You just ruined your own {TallestPyramidHeight}-story tall {PyramidBlock} pyramid. Sadge");
+                    TwitchClient.SpoolMessage($"@{userThatBrokePyramid} You just ruined your own {Pyramid.TallestHeight}-story tall {Pyramid.Block} pyramid. Sadge");
                 }
                 else
                 {
                     // User broke co-operative pyramid.
-                    TwitchClient.SpoolMessage($"@{userThatBrokePyramid} Imagine working with @{String.Join(", @", PyramidContributorUsernames.Where(name => !name.Equals(userThatBrokePyramid, StringComparison.OrdinalIgnoreCase)))} to build a {TallestPyramidHeight}-story tall {PyramidBlock} pyramid and then ruining it. 4WeirdW");
+                    TwitchClient.SpoolMessage($"@{userThatBrokePyramid} Imagine working with @{String.Join(", @", Pyramid.ContributorDisplayNames.Where(name => !name.Equals(userThatBrokePyramid, StringComparison.OrdinalIgnoreCase)))} to build a {Pyramid.TallestHeight}-story tall {Pyramid.Block} pyramid and then ruining it. 4WeirdW");
                 }
             }
             else
             {
                 // User broke someone else's pyramid.
-                TwitchClient.SpoolMessage($"@{userThatBrokePyramid} Nice job ruining the {TallestPyramidHeight}-story {PyramidBlock} tall pyramid built by @{String.Join(", @", PyramidContributorUsernames)}. PogO");
+                TwitchClient.SpoolMessage($"@{userThatBrokePyramid} Nice job ruining the {Pyramid.TallestHeight}-story {Pyramid.Block} tall pyramid built by @{String.Join(", @", Pyramid.ContributorDisplayNames)}. PogO");
 
             }
         }
