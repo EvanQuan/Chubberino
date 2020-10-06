372b371e3860027e7ffc9da85187175ffb896ba7
Evan Quan <evanlquan@gmail.com>
2020-10-05 21:00:05 -0600

    Add UsignRainbowColorSelector.WhenGettingNextColor

---
+++ Chubberino.UnitTests/Tests/Client/Commands/Settings/Colors/UsingRainbowColorSelector.cs
@@ -0,0 +1,14 @@
+﻿using Chubberino.Client.Commands.Settings.Colors;
+
+namespace Chubberino.UnitTests.Tests.Client.Commands.Settings.Colors
+{
+    public abstract class UsingRainbowColorSelector
+    {
+        protected RainbowColorSelector Sut { get; }
+
+        public UsingRainbowColorSelector()
+        {
+            Sut = new RainbowColorSelector();
+        }
+    }
+}
