e432979c9f7d1ace322094167a645dfb105bf99b
Evan Quan <evanlquan@gmail.com>
2020-10-05 21:27:15 -0600

    Add UsingPresetColorSelecotrWhenGettingNextColor

---
+++ Chubberino.UnitTests/Tests/Client/Commands/Settings/ColorSelectors/PresetColorSelectors/UsingPresetColorSelector.cs
@@ -0,0 +1,20 @@
+﻿using Chubberino.Client.Commands.Settings.ColorSelectors;
+using Moq;
+using System;
+
+namespace Chubberino.UnitTests.Tests.Client.Commands.Settings.ColorSelectors
+{
+    public abstract class UsingPresetColorSelector
+    {
+        protected PresetColorSelector Sut { get; }
+
+        protected Mock<Random> MockedRandom { get; }
+
+        public UsingPresetColorSelector()
+        {
+            MockedRandom = new Mock<Random>().SetupAllProperties();
+
+            Sut = new PresetColorSelector(MockedRandom.Object);
+        }
+    }
+}
