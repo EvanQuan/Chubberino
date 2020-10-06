04d61616614b356b79830e11a34c150909c13f25
Evan Quan <evanlquan@gmail.com>
2020-10-05 21:55:43 -0600

    Add UsingRandomColorSelector.WhenGettingNextColor

---
+++ Chubberino.UnitTests/Tests/Client/Commands/Settings/ColorSelectors/RandomColorSelectors/UsingRandomColorSelector.cs
@@ -0,0 +1,21 @@
+﻿using Chubberino.Client.Commands.Settings.ColorSelectors;
+using Moq;
+using System;
+
+namespace Chubberino.UnitTests.Tests.Client.Commands.Settings.ColorSelectors.RandomColorSelectors
+{
+    public abstract class UsingRandomColorSelector
+    {
+        protected RandomColorSelector Sut { get; }
+
+        protected Mock<Random> MockedRandom { get; }
+
+        public UsingRandomColorSelector()
+        {
+            MockedRandom = new Mock<Random>().SetupAllProperties();
+
+            Sut = new RandomColorSelector(MockedRandom.Object);
+        }
+
+    }
+}
---
+++ Chubberino.UnitTests/Tests/Client/Commands/Settings/ColorSelectors/RandomColorSelectors/WhenGettingNextColor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace Chubberino.UnitTests.Tests.Client.Commands.Settings.ColorSelectors.RandomColorSelectors
+{
+    public sealed class WhenGettingNextColor : UsingRandomColorSelector
+    {
+        [Theory]
+        [InlineData(0, "#000000")]
+        [InlineData(1, "#000001")]
+        [InlineData(10, "#00000A")]
+        [InlineData(15, "#00000F")]
+        [InlineData(16, "#000010")]
+        public void ShouldReturnRandomColor(Int32 randomNumber, String expectedColor)
+        {
+            MockedRandom.Setup(x => x.Next(0x1000000))
+                .Returns(randomNumber);
+
+            String color = Sut.GetNextColor();
+
+            Assert.Equal(expectedColor, color);
+        }
+    }
+}
