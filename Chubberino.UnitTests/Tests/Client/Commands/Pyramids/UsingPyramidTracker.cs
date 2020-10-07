85461bcbc4caae4ead4455a2b136964ae1d49f63
Evan Quan <evanlquan@gmail.com>
2020-10-06 23:58:13 -0600

    Add UsingPyramidTracker.WhenResetting

---
+++ Chubberino.UnitTests/Tests/Client/Commands/Pyramids/UsingPyramidTracker.cs
@@ -0,0 +1,17 @@
+﻿using Chubberino.Client.Commands.Settings.Pyramids;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chubberino.UnitTests.Tests.Client.Commands.Pyramids
+{
+    public abstract class UsingPyramidTracker
+    {
+        protected PyramidTracker Sut { get; }
+
+        public UsingPyramidTracker()
+        {
+            Sut = new PyramidTracker();
+        }
+    }
+}
---
+++ Chubberino.UnitTests/Tests/Client/Commands/Pyramids/WhenReseting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Chubberino.UnitTests.Tests.Client.Commands.Pyramids
+{
+    public sealed class WhenReseting : UsingPyramidTracker
+    {
+        [Fact]
+        public void ShouldResetValuesForSingleContributor()
+        {
+            Sut.Start("name", "block");
+
+            Sut.Reset();
+
+            Assert.Empty(Sut.ContributorDisplayNames);
+            Assert.Equal(0, Sut.CurrentHeight);
+            Assert.Equal(0, Sut.TallestHeight);
+            Assert.Null(Sut.Block);
+        }
+
+        [Fact]
+        public void ShouldResetValuesForMultipleContributors()
+        {
+            Sut.Start("name", "block");
+
+            Sut.BuildUp("name2");
+
+            Sut.Reset();
+
+            Assert.Empty(Sut.ContributorDisplayNames);
+            Assert.Equal(0, Sut.CurrentHeight);
+            Assert.Equal(0, Sut.TallestHeight);
+            Assert.Null(Sut.Block);
+        }
+    }
+}
