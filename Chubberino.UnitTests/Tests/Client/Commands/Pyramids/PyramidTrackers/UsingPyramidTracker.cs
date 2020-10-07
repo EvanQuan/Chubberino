b9f49b346852b891c76c09d15bc1715c82cc9ce8
Evan Quan <evanlquan@gmail.com>
2020-10-07 01:24:46 -0600

    Cleanup and formatting.

--- Chubberino.UnitTests/Tests/Client/Commands/Pyramids/PyramidTrackers/UsingPyramidTracker.cs
+++ Chubberino.UnitTests/Tests/Client/Commands/Pyramids/PyramidTrackers/UsingPyramidTracker.cs
@@ -1,15 +1,16 @@
 ﻿using Chubberino.Client.Commands.Settings.PyramidTrackers;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Chubberino.UnitTests.Tests.Client.Commands.Pyramids
 {
     public abstract class UsingPyramidTracker
     {
         protected const String ExpectedName1 = "1";
+
         protected const String ExpectedName2 = "2";
+
         protected const String ExpectedBlock1 = "a";
+
         protected const String ExpectedBlock2 = "b";
 
         protected PyramidTracker Sut { get; }
