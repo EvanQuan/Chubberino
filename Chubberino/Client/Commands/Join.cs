0d637ea1a36ee18a78f116d21dc53c3ac1b8f26e
Evan Quan <evanlquan@gmail.com>
2020-12-29 17:21:50 -0700

    Add CanJoinMultipleChannels

--- Chubberino/Client/Commands/Join.cs
+++ Chubberino/Client/Commands/Join.cs
@@ -1,5 +1,4 @@
 ﻿using Chubberino.Client.Abstractions;
-using Chubberino.Client.Extensions;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,6 +10,8 @@ namespace Chubberino.Client.Commands
     public sealed class Join : Command
     {
         private String JoinedChannelName { get; set; }
+
+        private Boolean CanJoinMultipleChannels { get; set; } = false;
 
         private IBot Bot { get; }
 
@@ -26,7 +27,7 @@ namespace Chubberino.Client.Commands
             // For simplicity, we can only be in 1 channel at a time.
             if (e.Channel.Equals(JoinedChannelName, StringComparison.OrdinalIgnoreCase)) { return; }
 
-            if (JoinedChannelName != null)
+            if (JoinedChannelName != null && !CanJoinMultipleChannels)
             {
                 TwitchClient.LeaveChannel(JoinedChannelName);
                 Console.WriteLine($"Left channel {JoinedChannelName}");
@@ -48,7 +49,22 @@ namespace Chubberino.Client.Commands
 
             String channelName = arguments.FirstOrDefault();
 
+            // If the user inputs any second argument, it will join that channel and not leave the existing channel.
+            CanJoinMultipleChannels = arguments.Skip(1).FirstOrDefault() != null;
+
             TwitchClient.EnsureJoinedToChannel(channelName);
+        }
+
+        public override string GetHelp()
+        {
+            return @"
+Join a twitch channel.
+
+usage: join <channel name> [allow multiple channels to join]
+
+    <channel name>                      Name of the twitch channel to join
+    [allow multiple channels to join]   Adding anything here will join this channel without leaving the current channel
+";
         }
     }
 }
