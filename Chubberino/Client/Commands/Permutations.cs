3e4ffbcf84d0ff21275fa35d13470aad1b0877e6
Evan Quan <evanlquan@gmail.com>
2021-02-07 05:57:59 -0700

    Add Permutations extension.

---
+++ Chubberino/Client/Commands/Permutations.cs
@@ -0,0 +1,29 @@
+﻿using Chubberino.Client.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chubberino.Client.Commands
+{
+    public sealed class Permutations : Command
+    {
+        public Permutations(IExtendedClient client, TextWriter console) : base(client, console)
+        {
+        }
+
+        public override void Execute(IEnumerable<String> arguments)
+        {
+            if (arguments.Count() == 0)
+            {
+                Console.WriteLine(GetHelp());
+                return;
+            }
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
--- Chubberino/Utility/IEnumerableExtensions.cs
+++ Chubberino/Utility/IEnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Chubberino.Utility
 {
@@ -13,6 +14,49 @@ namespace Chubberino.Utility
             }
 
             return source;
+        }
+
+        public static IEnumerable<String> GetPermutations(this String source)
+        {
+            var permutations = new List<String>();
+
+            if (source == String.Empty)
+            {
+                return permutations.Append(String.Empty);
+            }
+
+            Int32 maxRecursionDepth = source.Length - 1;
+
+            Char[] array = source.ToCharArray();
+
+            array.GetPermutationHelper(permutations, 0, maxRecursionDepth);
+
+            return permutations;
+        }
+
+        private static void GetPermutationHelper(this Char[] source, IList<String> permutations, Int32 currentRecursionDepth, Int32 maxRecursionDepth)
+        {
+            if (currentRecursionDepth == maxRecursionDepth)
+            {
+                permutations.Add(String.Join(String.Empty, source));
+                return;
+            }
+
+            for (Int32 i = currentRecursionDepth; i <= maxRecursionDepth; i++)
+            {
+                Swap(ref source[currentRecursionDepth], ref source[i]);
+                source.GetPermutationHelper(permutations, currentRecursionDepth + 1, maxRecursionDepth);
+                Swap(ref source[currentRecursionDepth], ref source[i]);
+            }
+        }
+
+        public static void Swap(ref Char a, ref Char b)
+        {
+            if (a == b) return;
+
+            var temp = a;
+            a = b;
+            b = temp;
         }
     }
 }
