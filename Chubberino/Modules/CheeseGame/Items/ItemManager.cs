20b6d86baf746ec1db3f0926640728cae5314fda
Evan Quan <evanlquan@gmail.com>
2021-02-13 06:01:39 -0700

    Reduce work price scaling

--- Chubberino/Modules/CheeseGame/Items/ItemManager.cs
+++ Chubberino/Modules/CheeseGame/Items/ItemManager.cs
@@ -10,7 +10,7 @@ namespace Chubberino.Modules.CheeseGame.Items
             return new PriceList(
                 storage: 25 + player.MaximumPointStorage / 2,
                 population: (Int32)(20 + Math.Pow(player.PopulationCount, 2)),
-                worker: (Int32)(100 + 5 * Math.Pow(player.WorkerCount, 2)),
+                worker: (Int32)(100 + 10 * Math.Pow(player.WorkerCount, 1.5)),
                 mousetrap: 50);
         }
     }
