edf7bbb23c9aa2db38b4c0f8e3b6c89ffa7846d5
Evan Quan <evanlquan@gmail.com>
2021-02-14 19:47:59 -0700

    Add channel specific SpoolMessage

--- Chubberino/Client/Abstractions/IMessageSpooler.cs
+++ Chubberino/Client/Abstractions/IMessageSpooler.cs
@@ -5,5 +5,7 @@ namespace Chubberino.Client.Abstractions
     public interface IMessageSpooler
     {
         void SpoolMessage(String message);
+
+        void SpoolMessage(String channelName, String message);
     }
 }
