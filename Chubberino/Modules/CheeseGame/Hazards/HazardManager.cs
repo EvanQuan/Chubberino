aecafc4fd9b38c7d488c3b769b795ab9ea0d6819
Evan Quan <evanlquan@gmail.com>
2021-02-14 22:06:19 -0700

    Change rat to mouse

--- Chubberino/Modules/CheeseGame/Hazards/HazardManager.cs
+++ Chubberino/Modules/CheeseGame/Hazards/HazardManager.cs
@@ -9,13 +9,13 @@ namespace Chubberino.Modules.CheeseGame.Hazards
 {
     public sealed class HazardManager : AbstractCommandStrategy, IHazardManager
     {
-        public const String NewInfestationMessage = "A giant rat sneaks into your factory, scaring away your workers. ";
+        public const String NewInfestationMessage = "A giant mouse sneaks into your factory, scaring away your workers. ";
 
-        public const String OldInfestationMessage = "A giant rat is still infesting your cheese factory, scaring away your workers. ";
+        public const String OldInfestationMessage = "A giant mouse is still infesting your cheese factory, scaring away your workers. ";
 
-        public const String KillOldRatMessage = "You set up a mousetrap, killing the giant rat infesting your cheese factory. Your workers go back to the work. ";
+        public const String KillOldRatMessage = "You set up a mousetrap, killing the giant mouse infesting your cheese factory. Your workers go back to the work. ";
 
-        public const String KillNewRatMessage = "A giant rat sneaks into your factory, but is promptly killed by a mousetrap you have set up. ";
+        public const String KillNewRatMessage = "A giant mouse sneaks into your factory, but is promptly killed by a mousetrap you have set up. ";
 
         public HazardManager(ApplicationContext context, IMessageSpooler spooler, Random random, IEmoteManager emoteManager) : base(context, spooler, random, emoteManager)
         {
