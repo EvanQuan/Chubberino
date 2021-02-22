20dd351b8cab2b694acc6d6ab6c32678f389e9c0
Evan Quan <evanlquan@gmail.com>
2021-02-22 15:29:38 -0700

    Reduce base quest success chance

--- Chubberino/Modules/CheeseGame/Constants.cs
+++ Chubberino/Modules/CheeseGame/Constants.cs
@@ -13,7 +13,7 @@ namespace Chubberino.Modules.CheeseGame
 
         public const Double WorkerUpgradePercent = 0.1;
 
-        public const Double QuestBaseSuccessChance = 0.25;
+        public const Double QuestBaseSuccessChance = 0.2;
 
         public const Double QuestSuccessUpgradePercent = 0.05;
 
