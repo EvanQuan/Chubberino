8c18f4344a16cb7d92512661fa82a616588133ed
Evan Quan <evanlquan@gmail.com>
2021-02-21 23:24:25 -0700

    Add migration

---
+++ Chubberino/Migrations/20210222062005_PlayerQuestSuccessUpgradeUnlock.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chubberino.Migrations
+{
+    public partial class PlayerQuestSuccessUpgradeUnlock : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NextQuestSuccessUpgradeUnlock",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NextQuestSuccessUpgradeUnlock",
+                table: "Players");
+        }
+    }
+}
