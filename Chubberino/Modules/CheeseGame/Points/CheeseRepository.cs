013be4057e7d798c406353bcf7af104b1df68bad
Evan Quan <evanlquan@gmail.com>
2021-02-21 22:00:01 -0700

    Add new cheese times. Remove remains.

--- Chubberino/Modules/CheeseGame/Points/CheeseRepository.cs
+++ Chubberino/Modules/CheeseGame/Points/CheeseRepository.cs
@@ -7,32 +7,46 @@ namespace Chubberino.Modules.CheeseGame.Points
 {
     public sealed class CheeseRepository : ICheeseRepository
     {
-        private const Int32 CheeseUnlockedOffset = 6;
+        private const Int32 CheeseUnlockedOffset = 5;
 
         private static IReadOnlyList<CheeseType> Cheeses { get; } = new List<CheeseType>()
         {
-            new CheeseType("Stinking Bishop", -6),
-            new CheeseType("Blue Castello", -2),
+            new CheeseType("Stinking Bishop", -5),
+            new CheeseType("Blue Castello", -1),
 
             new CheeseType("American", 2),
-            new CheeseType("Cheddar", 6),
-            new CheeseType("Mozzarella", 10),
-            new CheeseType("Swiss", 14),
+            new CheeseType("Cheddar", 4),
+            new CheeseType("Mozzarella", 6),
 
             // Unlockable cheese types
-            new CheeseType("Gouda", 18, Rank.Bronze, 100),
-            new CheeseType("Stilton", 22, Rank.Silver, 200),
-            new CheeseType("Brie", 26, Rank.Gold, 400),
-            new CheeseType("Mascarpone", 30, Rank.Platinum, 800),
+            new CheeseType("Swiss", 8, Rank.Bronze, 75),
+            new CheeseType("Babybel", 10, Rank.Bronze, 100),
+            new CheeseType("Gouda", 12, Rank.Bronze, 125),
+            new CheeseType("Monterey Jack", 14, Rank.Silver, 150),
+            new CheeseType("Asiago", 16, Rank.Silver, 200),
+            new CheeseType("Camembert", 18, Rank.Silver, 250),
+            new CheeseType("Grana", 20, Rank.Gold, 325),
+            new CheeseType("Brie", 22, Rank.Gold, 400),
+            new CheeseType("Roquefort", 24, Rank.Gold, 475),
+            new CheeseType("Emmental", 26, Rank.Platinum, 700),
+            new CheeseType("Mascarpone", 28, Rank.Platinum, 800),
+            new CheeseType("Bocconcini", 30, Rank.Platinum, 900),
+            new CheeseType("Halloumi", 32, Rank.Diamond, 1475),
             new CheeseType("Gorgonzola", 34, Rank.Diamond, 1600),
-            new CheeseType("Garroxta", 38, Rank.Master, 3200),
-            new CheeseType("Myzithra", 42, Rank.Grandmaster, 6400),
-            new CheeseType("Pule", 46, Rank.Legend, 12800),
+            new CheeseType("Reblochon", 36, Rank.Diamond, 1725),
+            new CheeseType("Oaxaca", 38, Rank.Master, 3050),
+            new CheeseType("Adelost", 40, Rank.Master, 3200),
+            new CheeseType("Garroxta", 42, Rank.Master, 3350),
+            new CheeseType("Juustoleipa", 44, Rank.Grandmaster, 6225),
+            new CheeseType("Castelmagno", 46, Rank.Grandmaster, 6400),
+            new CheeseType("Myzithra", 48, Rank.Grandmaster, 6575),
+            new CheeseType("Beaufort D'Ete", 50, Rank.Legend, 12600),
+            new CheeseType("Lord of the Hundreds", 52, Rank.Legend, 12800),
+            new CheeseType("Pule", 54, Rank.Legend, 13000),
         };
 
         public static IReadOnlyList<CheeseVariant> Variants { get; } = new List<CheeseVariant>()
         {
-            new CheeseVariant("some remains of", -1),
             new CheeseVariant("a slice of", 0),
             new CheeseVariant("a wheel of", 1),
             new CheeseVariant("a pile of", 2),
