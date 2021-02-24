edfa2a11db7d85e70cfd312958986d22042d3fb2
Evan Quan <evanlquan@gmail.com>
2021-02-24 14:08:26 -0700

    Add Int32Extensions

---
+++ Chubberino/Utility/Int32Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Chubberino.Utility
+{
+    public static class Int32Extensions
+    {
+        /// <summary>
+        /// Repeats the specified <paramref name="action"/> <paramref name="source"/> number of times.
+        /// </summary>
+        /// <param name="source">Number of times to repeat <paramref name="action"/>.</param>
+        /// <param name="action">Action to repeat.</param>
+        public static void Repeat(this Int32 source, Action action)
+        {
+            for (Int32 i = 0; i < source; i++)
+            {
+                action();
+            }
+        }
+
+        /// <summary>
+        /// Repeats the specified <paramref name="action"/> <paramref name="source"/> number of times, with the index of the iteration.
+        /// </summary>
+        /// <param name="source">Number of times to repeat <paramref name="action"/>.</param>
+        /// <param name="action">Action to repeat.</param>
+        public static void Repeat(this Int32 source, Action<Int32> action)
+        {
+            for (Int32 i = 0; i < source; i++)
+            {
+                action(i);
+            }
+        }
+    }
+}
