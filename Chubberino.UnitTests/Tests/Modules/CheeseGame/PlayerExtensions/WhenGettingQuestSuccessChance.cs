26f1ff501bfc16990ed449fdc7d0a03d3cdee332
Evan Quan <evanlquan@gmail.com>
2021-03-14 23:15:41 -0600

    Adjust tests

--- Chubberino.UnitTests/Tests/Modules/CheeseGame/PlayerExtensions/WhenGettingQuestSuccessChance.cs
+++ Chubberino.UnitTests/Tests/Modules/CheeseGame/PlayerExtensions/WhenGettingQuestSuccessChance.cs
@@ -27,8 +27,10 @@ namespace Chubberino.UnitTests.Tests.Modules.CheeseGame.PlayerExtensions
         [Theory]
         [InlineData(0, Rank.Bronze, 1, Rank.Bronze)]
         [InlineData(0, Rank.Bronze, 2, Rank.Silver)]
+        [InlineData(0, Rank.Silver, 2, Rank.Bronze)]
         [InlineData(1, Rank.Silver, 2, Rank.Silver)]
-        [InlineData(1, Rank.Silver, 1, Rank.Gold)]
+        [InlineData(1, Rank.Silver, 2, Rank.Gold)]
+        [InlineData(1, Rank.Gold, 2, Rank.Silver)]
         public void ShouldAddWorkerBonus(Int32 lesserWorkerCount, Rank lesserWorkerUpgrade, Int32 greaterWorkerCount, Rank greaterWorkerUpgrade)
         {
             Player.WorkerCount = lesserWorkerCount;
@@ -42,6 +44,25 @@ namespace Chubberino.UnitTests.Tests.Modules.CheeseGame.PlayerExtensions
             Double greaterResult = Player.GetQuestSuccessChance();
 
             Assert.True(lesserResult < greaterResult);
+        }
+
+        [Theory]
+        [InlineData(0, Rank.Bronze, Rank.Bronze)]
+        [InlineData(0, Rank.Bronze, Rank.Silver)]
+        [InlineData(1, Rank.Silver, Rank.Silver)]
+        [InlineData(1, Rank.Silver, Rank.Gold)]
+        public void ShouldNotBeImpactedByRank(Int32 workerCount, Rank lessRanker, Rank greaterRank)
+        {
+            Player.WorkerCount = workerCount;
+            Player.NextQuestRewardUpgradeUnlock = lessRanker;
+
+            Double lesserResult = Player.GetQuestSuccessChance();
+
+            Player.NextQuestRewardUpgradeUnlock = greaterRank;
+
+            Double greaterResult = Player.GetQuestSuccessChance();
+
+            Assert.Equal(lesserResult,  greaterResult);
         }
     }
 }
