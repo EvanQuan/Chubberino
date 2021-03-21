40818f8c01bda57129748bb99a96edfc66d05dbb
Evan Quan <evanlquan@gmail.com>
2021-03-21 00:35:03 -0600

    Make earlier cheeses unlockable

---
+++ Chubberino.UnitTests/Tests/Modules/CheeseGame/Points/CheeseRepositories/WhenGettingRandomCheese.cs
@@ -0,0 +1,88 @@
+﻿using Chubberino.Modules.CheeseGame.Points;
+using Moq;
+using System;
+using Xunit;
+
+namespace Chubberino.UnitTests.Tests.Modules.CheeseGame.Points.CheeseRepositories
+{
+    public sealed class WhenGettingRandomCheese : UsingCheeseRepository
+    {
+        public WhenGettingRandomCheese()
+        {
+            MockedRandom.Setup(x => x.Next(It.Is<Int32>(x => x < 0))).Throws<ArgumentOutOfRangeException>();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void ShouldReturnLowestCheese(Int32 cheeseUnlocked)
+        {
+            MockedRandom.Setup(x => x.Next(It.Is<Int32>(x => x >= 0))).Returns(0);
+
+            var result = Sut.GetRandomType(cheeseUnlocked);
+
+            var expectedCheese = CheeseRepository.Cheeses[0];
+
+            Assert.Equal(expectedCheese, result);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void ShouldReturnHighestUnlockedCheese(Int32 cheeseUnlocked)
+        {
+            MockedRandom.Setup(x => x.Next(It.IsAny<Int32>())).Returns<Int32>(x => x);
+
+            var result = Sut.GetRandomType(cheeseUnlocked);
+
+            var expectedCheese = CheeseRepository.Cheeses[cheeseUnlocked];
+
+            Assert.Equal(expectedCheese, result);
+        }
+
+        [Fact]
+        public void ShouldReturnHighestPossibleCheese()
+        {
+            Int32 maxCheeseUnlocked = CheeseRepository.Cheeses.Count - 1;
+
+            MockedRandom.Setup(x => x.Next(It.IsAny<Int32>())).Returns<Int32>(x => x);
+
+            var result = Sut.GetRandomType(maxCheeseUnlocked);
+
+            var lastCheese = CheeseRepository.Cheeses[CheeseRepository.Cheeses.Count - 1];
+
+            Assert.Equal(lastCheese, result);
+        }
+
+        [Fact]
+        public void ShouldReturnHighestPossibleCheeseForInvalidHighCheeseUnlocked()
+        {
+            Int32 maxCheeseUnlocked = CheeseRepository.Cheeses.Count;
+
+            MockedRandom.Setup(x => x.Next(It.IsAny<Int32>())).Returns<Int32>(x => x);
+
+            var result = Sut.GetRandomType(maxCheeseUnlocked);
+
+            var lastCheese = CheeseRepository.Cheeses[CheeseRepository.Cheeses.Count - 1];
+
+            Assert.Equal(lastCheese, result);
+        }
+
+        [Fact]
+        public void ShouldReturnLowestPossibleCheeseForInvalidLowCheeseUnlocked()
+        {
+            Int32 invalidCheeseUnlocked = -1;
+
+            MockedRandom.Setup(x => x.Next(It.Is<Int32>(x => x >= 0))).Returns(0);
+
+            var result = Sut.GetRandomType(invalidCheeseUnlocked);
+
+            var firstCheese = CheeseRepository.Cheeses[0];
+
+            Assert.Equal(firstCheese, result);
+        }
+
+    }
+}
--- Chubberino/Modules/CheeseGame/Points/CheeseRepository.cs
+++ Chubberino/Modules/CheeseGame/Points/CheeseRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Chubberino.Modules.CheeseGame.Models;
 using Chubberino.Modules.CheeseGame.Rankings;
+using Chubberino.Utility;
 using System;
 using System.Collections.Generic;
 
@@ -7,19 +8,19 @@ namespace Chubberino.Modules.CheeseGame.Points
 {
     public sealed class CheeseRepository : ICheeseRepository
     {
-        private const Int32 CheeseUnlockedOffset = 4;
-
-        private static IReadOnlyList<CheeseType> Cheeses { get; } = new List<CheeseType>()
+        public static IReadOnlyList<CheeseType> Cheeses { get; } = new List<CheeseType>()
         {
+            // Players cannot have an unlock level equal to a value immediately
+            // before a negative cheese.
             new CheeseType("Spray", 1),
-            new CheeseType("Spread", 2),
-            new CheeseType("Velveeta", 3),
-            new CheeseType("American", 4), // 0
+            new CheeseType("Spread", 2, Rank.Bronze, 5),
+            new CheeseType("Velveeta", 3, Rank.Bronze, 10),
+            new CheeseType("American", 4, Rank.Bronze, 15),
 
             // Unlockable cheese types
-            new CheeseType("Cheddar", 5, Rank.Bronze, 50, true), // 1
+            new CheeseType("Cheddar", 5, Rank.Bronze, 30, true), // 4
             new CheeseType("Blue Castello", -1),
-            new CheeseType("Mozzarella", 6, Rank.Bronze, 60),
+            new CheeseType("Mozzarella", 6, Rank.Bronze, 50),
             new CheeseType("Friulano", 7, Rank.Bronze, 70),
             new CheeseType("Swiss", 8, Rank.Bronze, 80),
             new CheeseType("Tetilla", 9, Rank.Bronze, 90),
@@ -27,7 +28,7 @@ namespace Chubberino.Modules.CheeseGame.Points
             new CheeseType("Piave", 11, Rank.Bronze, 110),
             new CheeseType("Gouda", 12, Rank.Bronze, 120),
 
-            new CheeseType("Havarti", 13, Rank.Silver, 140, true), // 10
+            new CheeseType("Havarti", 13, Rank.Silver, 140, true), // 13
             new CheeseType("Stinking Bishop", -5),
             new CheeseType("Monterey Jack", 14, Rank.Silver, 160),
             new CheeseType("Morbier", 15, Rank.Silver, 180),
@@ -35,7 +36,7 @@ namespace Chubberino.Modules.CheeseGame.Points
             new CheeseType("Red Cloud", 17, Rank.Silver, 220),
             new CheeseType("Camembert", 18, Rank.Silver, 240),
 
-            new CheeseType("Dry Jack", 19, Rank.Gold, 280, true), // 17
+            new CheeseType("Dry Jack", 19, Rank.Gold, 280, true), // 20
             new CheeseType("Cheshire", -13),
             new CheeseType("Grana", 20, Rank.Gold, 320),
             new CheeseType("Kasseri", 21, Rank.Gold, 360),
@@ -43,7 +44,7 @@ namespace Chubberino.Modules.CheeseGame.Points
             new CheeseType("Parmesan", 23, Rank.Gold, 440),
             new CheeseType("Roquefort", 24, Rank.Gold, 480),
 
-            new CheeseType("Kasseri", 25, Rank.Platinum, 560, true), // 24
+            new CheeseType("Kasseri", 25, Rank.Platinum, 560, true), // 27
             new CheeseType("Limburger", -19),
             new CheeseType("Emmental", 26, Rank.Platinum, 640),
             new CheeseType("Ogleshield", 27, Rank.Platinum, 720),
@@ -51,7 +52,7 @@ namespace Chubberino.Modules.CheeseGame.Points
             new CheeseType("Wensleydale", 29, Rank.Platinum, 880),
             new CheeseType("Bocconcini", 30, Rank.Platinum, 960),
 
-            new CheeseType("Neufchatel", 31, Rank.Diamond, 1120, true), // 31
+            new CheeseType("Neufchatel", 31, Rank.Diamond, 1120, true), // 34
             new CheeseType("Oxford Blue", -25),
             new CheeseType("Halloumi", 32, Rank.Diamond, 1280),
             new CheeseType("Stilton", 33, Rank.Diamond, 1440),
@@ -59,7 +60,7 @@ namespace Chubberino.Modules.CheeseGame.Points
             new CheeseType("Lairobell", 35, Rank.Diamond, 1760),
             new CheeseType("Reblochon", 36, Rank.Diamond, 1920),
 
-            new CheeseType("Edam", 37, Rank.Master, 2240, true), // 38
+            new CheeseType("Edam", 37, Rank.Master, 2240, true), // 41
             new CheeseType("Muenster", -31),
             new CheeseType("Oaxaca", 38, Rank.Master, 2560),
             new CheeseType("Ulloa", 39, Rank.Master, 2880),
@@ -67,7 +68,7 @@ namespace Chubberino.Modules.CheeseGame.Points
             new CheeseType("Montagnolo", 41, Rank.Master, 3520),
             new CheeseType("Garroxta", 42, Rank.Master, 3840),
 
-            new CheeseType("Cabrales", 43, Rank.Grandmaster, 4480, true), // 45
+            new CheeseType("Cabrales", 43, Rank.Grandmaster, 4480, true), // 48
             new CheeseType("Danablu", -37),
             new CheeseType("Juustoleipa", 44, Rank.Grandmaster, 5120),
             new CheeseType("Flagship Block", 45, Rank.Grandmaster, 5760),
@@ -75,13 +76,14 @@ namespace Chubberino.Modules.CheeseGame.Points
             new CheeseType("Gammelost", 47, Rank.Grandmaster, 7040),
             new CheeseType("Myzithra", 48, Rank.Grandmaster, 7680),
 
-            new CheeseType("Beaufort D'Ete", 49, Rank.Legend, 8960, true), // 52
+            new CheeseType("Beaufort D'Ete", 49, Rank.Legend, 8960, true), // 55
             new CheeseType("Epoisses", -43),
             new CheeseType("Winnimere", 50, Rank.Legend, 10240),
             new CheeseType("Cacio Bufala", 51, Rank.Legend, 11520),
             new CheeseType("Lord of the Hundreds", 52, Rank.Legend, 12800),
             new CheeseType("White Stilton Gold", 53, Rank.Legend, 14080),
             new CheeseType("Pule", 54, Rank.Legend, 15360),
+            new CheeseType("Chubmeister", 100, Rank.Legend, 16000),
         };
 
         public CheeseRepository(Random random)
@@ -93,13 +95,14 @@ namespace Chubberino.Modules.CheeseGame.Points
 
         public Boolean TryGetNextCheeseToUnlock(Player player, out CheeseType cheeseType)
         {
-            if (player.CheeseUnlocked + CheeseUnlockedOffset >= Cheeses.Count)
+            Int32 nextCheese = player.CheeseUnlocked + 1;
+            if (nextCheese >= Cheeses.Count)
             {
                 cheeseType = default;
                 return false;
             }
 
-            cheeseType = Cheeses[CheeseUnlockedOffset + player.CheeseUnlocked];
+            cheeseType = Cheeses[nextCheese];
             return true;
         }
 
@@ -110,7 +113,7 @@ namespace Chubberino.Modules.CheeseGame.Points
 
         private CheeseType GetRandomBaseType(Int32 cheeseUnlocked)
         {
-            return Cheeses[Random.Next(Math.Min(Cheeses.Count, CheeseUnlockedOffset + cheeseUnlocked))];
+            return Cheeses[Random.Next(cheeseUnlocked.Min(Cheeses.Count - 1).Max(0))];
         }
     }
 }
