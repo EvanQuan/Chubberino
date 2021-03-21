10ecbb2fcd3ce46bc4304e71bdd3b13f848b3bd9
Evan Quan <evanlquan@gmail.com>
2021-03-21 02:10:30 -0600

    Add MockedRandomSetupExtensions

---
+++ Chubberino.UnitTests/Utility/MockedRandomSetupExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using System;
+
+namespace Chubberino.UnitTests.Utility
+{
+    public static class MockedRandomSetupExtensions
+    {
+        public static Mock<Random> SetupReturnMinimum(this Mock<Random> random)
+        {
+            random.SetupThrowOutOfRandomExceptionOnNegative();
+            random.Setup(x => x.Next(It.Is<Int32>(x => x >= 0), It.Is<Int32>(x => x >= 0))).Returns<Int32, Int32>((min, max) => min);
+            random.Setup(x => x.Next()).Returns(0);
+            random.Setup(x => x.Next(It.Is<Int32>(x => x >= 0))).Returns(0);
+            random.Setup(x => x.NextDouble()).Returns(0);
+
+            return random;
+        }
+
+        public static Mock<Random> SetupReturnMaximum(this Mock<Random> random)
+        {
+            random.SetupThrowOutOfRandomExceptionOnNegative();
+            random.Setup(x => x.Next(It.Is<Int32>(x => x >= 0), It.Is<Int32>(x => x >= 0))).Returns<Int32, Int32>((min, max) => max);
+            random.Setup(x => x.Next()).Returns(0);
+            random.Setup(x => x.Next(It.Is<Int32>(x => x >= 0))).Returns<Int32>(x => x);
+            random.Setup(x => x.NextDouble()).Returns(1);
+
+            return random;
+        }
+
+        public static Mock<Random> SetupThrowOutOfRandomExceptionOnNegative(this Mock<Random> random)
+        {
+            random.Setup(x => x.Next(It.Is<Int32>(x => x < 0), It.IsAny<Int32>())).Throws<ArgumentOutOfRangeException>();
+            random.Setup(x => x.Next(It.IsAny<Int32>(), It.Is<Int32>(x => x < 0))).Throws<ArgumentOutOfRangeException>();
+            random.Setup(x => x.Next(It.Is<Int32>(x => x < 0))).Throws<ArgumentOutOfRangeException>();
+
+            return random;
+        }
+    }
+}
