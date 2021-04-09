06d7dfdd1b20950e469dc3c8e6bc11f60d9c56a1
Evan Quan <evanlquan@gmail.com>
2021-04-08 18:21:39 -0600

    TryValidCommand checks for first word instead of StartsWith

---
+++ Chubberino.UnitTests/Tests/Client/Commands/Settings/UserCommands/UsingUserCommand.cs
@@ -0,0 +1,35 @@
+﻿using Chubberino.Client;
+using Chubberino.Client.Commands.Settings.UserCommands;
+using Moq;
+using System;
+using System.Collections.Generic;
+using TwitchLib.Client.Events;
+
+namespace Chubberino.UnitTests.Tests.Client.Commands.Settings.UserCommands
+{
+    public abstract class UsingUserCommand
+    {
+        protected Mock<ITwitchClientManager> MockedClient { get; }
+
+        protected Mock<IConsole> MockedConsole { get; }
+
+        protected UserCommandWrapper Sut { get; }
+
+        protected UsingUserCommand()
+        {
+            MockedClient = new Mock<ITwitchClientManager>();
+            MockedConsole = new Mock<IConsole>();
+
+            Sut = new UserCommandWrapper(MockedClient.Object, MockedConsole.Object);
+        }
+
+        protected sealed class UserCommandWrapper : UserCommand
+        {
+            public UserCommandWrapper(ITwitchClientManager client, IConsole console) : base(client, console)
+            {
+            }
+
+            public new Boolean TryValidateCommand(OnMessageReceivedArgs args, out IEnumerable<String> words) => base.TryValidateCommand(args, out words);
+        }
+    }
+}
---
+++ Chubberino.UnitTests/Tests/Client/Commands/Settings/UserCommands/WhenTryValidatingCommand.cs
@@ -0,0 +1,76 @@
+﻿using Chubberino.Client.Commands.Settings.UserCommands;
+using System;
+using System.Linq;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models.Builders;
+using Xunit;
+
+namespace Chubberino.UnitTests.Tests.Client.Commands.Settings.UserCommands
+{
+    public sealed class WhenTryValidatingCommand : UsingUserCommand
+    {
+        [Theory]
+        [InlineData("!usercommandwrapper", new String[] { })]
+        [InlineData("!userCommandWrapper", new String[] { })]
+        [InlineData("!userCommandWrapper a", new String[] { "a" })]
+        [InlineData("!userCommandWrapper a b", new String[] { "a", "b" })]
+        public void ShouldValidateCommand(String input, String[] expectedWords)
+        {
+            Boolean result = Sut.TryValidateCommand(new OnMessageReceivedArgs()
+            {
+                ChatMessage = ChatMessageBuilder.Create()
+                .WithMessage(input)
+                .Build()
+            },
+            out var words);
+
+            Assert.True(result);
+            Assert.True(expectedWords.SequenceEqual(words));
+        }
+
+        [Theory]
+        [InlineData("!usercommandwrapper")]
+        [InlineData("!userCommandWrapper")]
+        [InlineData("!userCommandWrapper a")]
+        [InlineData("!userCommandWrapper a b")]
+        public void ShouldIgnoreUser(String input)
+        {
+            foreach (String id in UserCommand.UserIdsToIgnore)
+            {
+                Boolean result = Sut.TryValidateCommand(new OnMessageReceivedArgs()
+                {
+                    ChatMessage = ChatMessageBuilder.Create()
+                    .WithTwitchLibMessage(TwitchLibMessageBuilder.Create().WithUserId(id).Build())
+                    .WithMessage(input)
+                    .Build()
+                },
+                out var words);
+
+                Assert.False(result);
+                Assert.Null(words);
+            }
+        }
+
+        [Theory]
+        [InlineData("!a")]
+        [InlineData("!usercommandwrappera")]
+        [InlineData("usercommandwrapper")]
+        [InlineData("userCommandWrapper")]
+        [InlineData("!userCommandWrappera")]
+        [InlineData("!userCommandWrappera a")]
+        [InlineData("!userCommandWrappera a b")]
+        public void ShouldInvalidateCommand(String input)
+        {
+            Boolean result = Sut.TryValidateCommand(new OnMessageReceivedArgs()
+            {
+                ChatMessage = ChatMessageBuilder.Create()
+                .WithMessage(input)
+                .Build()
+            },
+            out var words);
+
+            Assert.False(result);
+            Assert.Null(words);
+        }
+    }
+}
