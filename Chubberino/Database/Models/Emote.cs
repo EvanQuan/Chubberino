0ad08212c1a2d57206b6808d73714b1578adf27b
Evan Quan <evanlquan@gmail.com>
2021-04-25 02:11:01 -0600

    Add TwitchDisplayName

--- Chubberino/Database/Models/Emote.cs
+++ Chubberino/Database/Models/Emote.cs
@@ -6,13 +6,24 @@ namespace Chubberino.Database.Models
     {
         public Int32 ID { get; set; }
 
-        public String Name { get; set;  }
+        /// <summary>
+        /// Emote name.
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Twitch display name.
+        /// </summary>
+        public String TwitchDisplayName { get; set; }
 
         /// <summary>
         /// The Twitch user ID of the user that has the emote enabled.
         /// </summary>
         public String TwitchUserID { get; set; }
 
+        /// <summary>
+        /// Emote category.
+        /// </summary>
         public EmoteCategory Category { get; set; }
     }
 }
