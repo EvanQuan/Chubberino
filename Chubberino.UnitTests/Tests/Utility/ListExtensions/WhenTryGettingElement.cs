1e098f72e8fb7bb312b27c896b30148af6bd0429
Evan Quan <evanlquan@gmail.com>
2021-05-11 08:25:58 -0600

    Cleanup usings.

--- Chubberino.UnitTests/Tests/Utility/ListExtensions/WhenTryGettingElement.cs
+++ Chubberino.UnitTests/Tests/Utility/ListExtensions/WhenTryGettingElement.cs
@@ -1,9 +1,6 @@
 ﻿using Chubberino.Utility;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Chubberino.UnitTests.Tests.Utility.ListExtensions
