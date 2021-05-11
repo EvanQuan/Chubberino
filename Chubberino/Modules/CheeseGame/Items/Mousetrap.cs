3f7b7f91c63a9bbc4d5dc3b2824ce810ada17301
Evan Quan <evanlquan@gmail.com>
2021-05-11 08:32:13 -0600

    Add Mousetrap

---
+++ Chubberino/Modules/CheeseGame/Items/Mousetrap.cs
@@ -0,0 +1,35 @@
+﻿using Chubberino.Modules.CheeseGame.Models;
+using Monad;
+using System;
+using System.Collections.Generic;
+
+namespace Chubberino.Modules.CheeseGame.Items
+{
+    public sealed class Mousetrap : Item
+    {
+        public override IEnumerable<String> Names { get; } = new String[] { "Mousetrap", "m", "mouse", "mousetraps" };
+
+        public override Int32 GetPrice(Player player)
+        {
+            return 25;
+        }
+
+        public override String GetSpecificNameForNotEnoughToBuy(Player player)
+        {
+            return "a mousetrap";
+        }
+
+        public override String GetSpecificNameForSuccessfulBuy(Player player, Int32 quantity)
+        {
+            return $"{quantity} mousetrap{(quantity == 1 ? String.Empty : "s")}";
+        }
+
+        public override Either<Int32, String> TryBuySingleUnit(Player player, Int32 price)
+        {
+            player.MouseTrapCount++;
+            player.Points -= price;
+
+            return () => 1;
+        }
+    }
+}
