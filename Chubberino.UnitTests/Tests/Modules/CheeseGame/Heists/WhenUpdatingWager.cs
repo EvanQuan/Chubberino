e433aa9509e049c26daba06be63d0fec819f3808
Evan Quan <evanlquan@gmail.com>
2021-05-11 12:03:29 -0600

    Add test.

--- Chubberino.UnitTests/Tests/Modules/CheeseGame/Heists/WhenUpdatingWager.cs
+++ Chubberino.UnitTests/Tests/Modules/CheeseGame/Heists/WhenUpdatingWager.cs
@@ -61,6 +61,38 @@ namespace Chubberino.UnitTests.Tests.Modules.CheeseGame.Heists
             Assert.Equal(expectedPlayerPointsAfterWager, Player.Points);
         }
 
+        /// <summary>
+        /// When updating a wager to the same value as it was before, no
+        /// message should appear in response.
+        /// </summary>
+        /// <param name="playerStorage"></param>
+        /// <param name="playerPoints"></param>
+        /// <param name="initialPointsWagered"></param>
+        /// <param name="newPointsWagered"></param>
+        /// <param name="expectedNewPointsWagered"></param>
+        /// <param name="expectedPlayerPointsAfterWager"></param>
+        [Theory]
+        [InlineData(2, 2, 2, 2, 2, 0)]
+        [InlineData(2, 2, 1, 1, 1, 1)]
+        public void ShouldNotRespondToNonChangingWagerUpdate(
+            Int32 playerStorage,
+            Int32 playerPoints,
+            Int32 initialPointsWagered,
+            Int32 newPointsWagered,
+            Int32 expectedNewPointsWagered,
+            Int32 expectedPlayerPointsAfterWager)
+        {
+            Player.MaximumPointStorage = playerStorage;
+            Player.Points = playerPoints;
+            Sut.Wagers.Add(new Wager(Player.TwitchUserID, initialPointsWagered));
+
+            Sut.UpdateWager(Player, p => newPointsWagered);
+
+            Assert.Contains(Sut.Wagers, x => x.PlayerTwitchID.Equals(Player.TwitchUserID) && x.WageredPoints == expectedNewPointsWagered);
+            MockedTwitchClientManager.Verify(x => x.SpoolMessage(ChatMessage.Channel, It.IsAny<String>(), It.IsAny<Priority>()), Times.Never());
+            Assert.Equal(expectedPlayerPointsAfterWager, Player.Points);
+        }
+
         [Theory]
         [InlineData(1, 0, 2, 0, 1)]
         [InlineData(1, 0, 2, -1, 1)]
