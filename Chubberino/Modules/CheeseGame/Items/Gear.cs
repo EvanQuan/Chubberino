f91cb790edfa060109aee070f668e5bc2bd1584e
Evan Quan <evanlquan@gmail.com>
2021-05-11 08:26:02 -0600

    Add Gear

---
+++ Chubberino/Modules/CheeseGame/Items/Gear.cs
@@ -0,0 +1,35 @@
+﻿using Chubberino.Modules.CheeseGame.Models;
+using Monad;
+using System;
+using System.Collections.Generic;
+
+namespace Chubberino.Modules.CheeseGame.Items
+{
+    public sealed class Gear : Item
+    {
+        public override IEnumerable<String> Names => new String[] { "Gear", "g", "gears" };
+
+        public override Int32 GetPrice(Player player)
+        {
+            return 5 + 5 * player.GearCount;
+        }
+
+        public override String GetSpecificNameForNotEnoughToBuy(Player player)
+        {
+            return "some gear";
+        }
+
+        public override String GetSpecificNameForSuccessfulBuy(Player player, Int32 quantity)
+        {
+            return $"{quantity} gear";
+        }
+
+        public override Either<Int32, String> TryBuySingleUnit(Player player, Int32 price)
+        {
+            player.GearCount++;
+            player.Points -= price;
+
+            return () => 1;
+        }
+    }
+}
