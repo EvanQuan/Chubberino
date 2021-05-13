400b813df5eae39ef66c387c47671783e4baeacd
Evan Quan <evanlquan@gmail.com>
2021-05-13 03:34:16 -0600

    Add WhenTryGettingNextCheeseTypeToUnlock tests

---
+++ Chubberino.UnitTests/Tests/Modules/CheeseGame/Points/CheeseRepositories/WhenTryGettingNextCheeseTypeToUnlock.cs
@@ -0,0 +1,50 @@
+﻿using Chubberino.Modules.CheeseGame.Models;
+using Chubberino.Modules.CheeseGame.Points;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Chubberino.UnitTests.Tests.Modules.CheeseGame.Points.CheeseRepositories
+{
+    public sealed class WhenTryGettingNextCheeseTypeToUnlock
+    {
+        public IReadOnlyList<CheeseType> Sut = CheeseRepository.Cheeses;
+
+        private Player Player { get; }
+
+        public WhenTryGettingNextCheeseTypeToUnlock()
+        {
+            Player = new Player();
+        }
+
+        [Fact]
+        public void ShouldReturnSecondCheese()
+        {
+            var result = Sut.TryGetNextToUnlock(Player, out var cheeseType);
+
+            Assert.True(result);
+            Assert.NotNull(cheeseType);
+            Assert.Equal(Sut[1], cheeseType);
+        }
+
+        [Fact]
+        public void ShouldReturnLastCheese()
+        {
+            Player.CheeseUnlocked = Sut.Count - 2;
+            var result = Sut.TryGetNextToUnlock(Player, out var cheeseType);
+
+            Assert.True(result);
+            Assert.NotNull(cheeseType);
+            Assert.Equal(Sut[Sut.Count - 1], cheeseType);
+        }
+
+        [Fact]
+        public void ShouldReturnNoCheese()
+        {
+            Player.CheeseUnlocked = Sut.Count;
+            var result = Sut.TryGetNextToUnlock(Player, out var cheeseType);
+
+            Assert.False(result);
+            Assert.Null(cheeseType);
+        }
+    }
+}
