1774a2ac81196d901ae09872597fd2389c98f998
Evan Quan <evanlquan@gmail.com>
2021-05-18 12:33:28 -0600

    Add RandomQuestRepositoryExtensions

---
+++ Chubberino/Modules/CheeseGame/Quests/RandomQuestRepositoryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Chubberino.Utility;
+using System;
+
+namespace Chubberino.Modules.CheeseGame.Quests
+{
+    public static class RandomQuestRepositoryExtensions
+    {
+        public static Quest NextElement(this Random random, IQuestRepository questRepository)
+        {
+            return random.TryPercentChance(questRepository.RareQuestChance)
+                ? random.NextElement(questRepository.RareQuests)
+                : random.NextElement(questRepository.CommonQuests);
+        }
+    }
+}
