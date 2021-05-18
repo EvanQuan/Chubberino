f9da9772ab7b8b030c8e1707dc162a77458632d2
Evan Quan <evanlquan@gmail.com>
2021-05-18 13:25:22 -0600

    Add extra prompt text for rare quest

--- Chubberino/Modules/CheeseGame/Quests/RandomQuestRepositoryExtensions.cs
+++ Chubberino/Modules/CheeseGame/Quests/RandomQuestRepositoryExtensions.cs
@@ -5,11 +5,11 @@ namespace Chubberino.Modules.CheeseGame.Quests
 {
     public static class RandomQuestRepositoryExtensions
     {
-        public static Quest NextElement(this Random random, IQuestRepository questRepository)
+        public static Quest NextElement(this Random random, IQuestRepository questRepository, Int32 unlockedCount)
         {
             return random.TryPercentChance(questRepository.RareQuestChance)
                 ? random.NextElement(questRepository.RareQuests)
-                : random.NextElement(questRepository.CommonQuests);
+                : random.NextElement(questRepository.CommonQuests, unlockedCount);
         }
     }
 }
