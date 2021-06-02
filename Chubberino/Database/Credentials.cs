0a217ebdb1f99be812280f4c2f4ad8f2a4ec1540
Evan Quan <evanlquan@gmail.com>
2021-06-02 02:06:44 -0600

    Move Credential resolution to ICredentials

---
+++ Chubberino/Database/Credentials.cs
@@ -0,0 +1,23 @@
+﻿using Chubberino.Database.Contexts;
+using Chubberino.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Chubberino.Database
+{
+    public sealed class Credentials
+    {
+        public Credentials(IApplicationContextFactory applicationContextFactory)
+        {
+            using var context = applicationContextFactory.GetContext();
+
+            UserCredentials = context.UserCredentials.AsNoTracking().First();
+
+            ApplicationCredentials = context.ApplicationCredentials.AsNoTracking().First();
+        }
+
+        public UserCredentials UserCredentials { get; }
+
+        public ApplicationCredentials ApplicationCredentials { get; }
+    }
+}
