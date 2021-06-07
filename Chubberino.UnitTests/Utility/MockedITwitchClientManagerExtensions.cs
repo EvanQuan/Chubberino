4072df59e85388c5ab94e93597d6a96bd48e0329
Evan Quan <evanlquan@gmail.com>
2021-06-07 10:26:55 -0600

    Add MockedITwitchClientManagerExtensions

---
+++ Chubberino.UnitTests/Utility/MockedITwitchClientManagerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Chubberino.Client;
+using Moq;
+using TwitchLib.Client.Interfaces;
+
+namespace Chubberino.UnitTests.Utility
+{
+    public static class MockedITwitchClientManagerExtensions
+    {
+        public static Mock<ITwitchClient> SetupClient(this Mock<ITwitchClientManager> source)
+        {
+            Mock<ITwitchClient> client = new();
+
+            source.Setup(x => x.Client).Returns(client.Object);
+
+            return client;
+        }
+    }
+}
