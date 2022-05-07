abde0e42139c133d73b73ccd71415183b253a8d8
Evan Quan <evanlquan@gmail.com>
2022-05-06 22:53:48 -0600

    Change to record.

--- Chubberino.Bots.Channel/Modules/CheeseGame/Items/BuyResult.cs
+++ Chubberino.Bots.Channel/Modules/CheeseGame/Items/BuyResult.cs
@@ -1,20 +1,5 @@
 ﻿using System;
 
-namespace Chubberino.Modules.CheeseGame.Items
-{
-    public struct BuyResult
-    {
-        public BuyResult(Int32 quantityPurchased, Int32 pointsSpent, String extraMessage)
-        {
-            QuantityPurchased = quantityPurchased;
-            PointsSpent = pointsSpent;
-            ExtraMessage = extraMessage;
-        }
+namespace Chubberino.Modules.CheeseGame.Items;
 
-        public Int32 QuantityPurchased { get; }
-
-        public Int32 PointsSpent { get; }
-
-        public String ExtraMessage { get; }
-    }
-}
+public readonly record struct BuyResult(Int32 QuantityPurchased, Int32 PointsSpent, String ExtraMessage);
