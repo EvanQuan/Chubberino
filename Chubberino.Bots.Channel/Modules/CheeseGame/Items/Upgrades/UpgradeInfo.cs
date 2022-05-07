d96c033475099fa2566ad8a39efab01d6e9af3a0
Evan Quan <evanlquan@gmail.com>
2022-05-06 23:19:41 -0600

    File-scope namespace.

--- Chubberino.Bots.Channel/Modules/CheeseGame/Items/Upgrades/UpgradeInfo.cs
+++ Chubberino.Bots.Channel/Modules/CheeseGame/Items/Upgrades/UpgradeInfo.cs
@@ -2,25 +2,24 @@
 using Chubberino.Modules.CheeseGame.Ranks;
 using System;
 
-namespace Chubberino.Modules.CheeseGame.Items.Upgrades
+namespace Chubberino.Modules.CheeseGame.Items.Upgrades;
+
+public sealed class UpgradeInfo
 {
-    public class UpgradeInfo
+    public UpgradeInfo(String description, Rank rankToUnlock, Double rankPricePercentPrice, Action<Player> updatePlayer)
     {
-        public UpgradeInfo(String description, Rank rankToUnlock, Double rankPricePercentPrice, Action<Player> updatePlayer)
-        {
-            Description = description;
-            RankToUnlock = rankToUnlock;
-            Price = (Int32)(RankManager.RanksToPoints[RankToUnlock] * rankPricePercentPrice);
-            UpdatePlayer = updatePlayer;
-        }
-
-        public String Description { get; }
-
-
-        public Rank RankToUnlock { get; }
-
-        public Int32 Price { get; }
-
-        public Action<Player> UpdatePlayer { get; }
+        Description = description;
+        RankToUnlock = rankToUnlock;
+        Price = (Int32)(RankManager.RanksToPoints[RankToUnlock] * rankPricePercentPrice);
+        UpdatePlayer = updatePlayer;
     }
+
+    public String Description { get; }
+
+
+    public Rank RankToUnlock { get; }
+
+    public Int32 Price { get; }
+
+    public Action<Player> UpdatePlayer { get; }
 }
